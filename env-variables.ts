57afa2a5f743d7b5d0c7929fdb3d8d2f1fba6110
Viljami Vastamäki <viljami.vastamaki@tutanota.com>
2022-08-12 15:12:16 +0300

    Improve get environment variables function

--- env-variables.ts
+++ env-variables.ts
@@ -7,8 +7,21 @@ const variables = {
 
 export const getEnvVariables = (stage: string) => {
   const vars = {};
-  Object.keys(variables).forEach((key: string) => {
+  
+  Object.keys(variables).forEach((key) => {
+    const type = typeof variables[key];
+
+    const isTypeString = type === "string";
+
+    const isObjectWithSingleKey =
+      type === "object" && Object.keys(variables[key]).length === 1;
+
+    if (isTypeString || isObjectWithSingleKey) {
+      return (vars[key] = variables[key]);
+    }
+
     vars[key] = variables[key][stage];
   });
+
   return vars;
 };
